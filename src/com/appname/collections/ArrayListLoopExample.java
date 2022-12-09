495c00ff03d001911a8e92192b89e879266409ed
Ritesh Mansukhani <riteshmansukhani@gmail.com>
2022-12-09 10:11:00 +0530

    Collection Programs Update

--- src/com/appname/collections/ArrayListLoopExample.java
+++ src/com/appname/collections/ArrayListLoopExample.java
@@ -17,5 +17,18 @@ public class ArrayListLoopExample {
         System.out.println(arrayListObj1);
         
         
+        List<String> arrayObj = Arrays.asList("He","Is","A","Jolly","Good","Fellow");
+        //Using Advance For Loop
+        for (String eachWord : arrayObj) {
+        	System.out.print(eachWord+" ");
+        }
+        
+        //Using Iterator
+        Iterator <String> iteratorObj= arrayObj.iterator();
+        //Print New Line to have next output on fresh line
+        System.out.println();
+        while (iteratorObj.hasNext()) {
+        	System.out.print(iteratorObj.next()+" ");
+        }
     }
 }

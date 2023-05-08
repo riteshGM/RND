3f5d7f267812be8e3f20c6baa37801c762e23226
Ritesh Mansukhani <riteshmansukhani@gmail.com>
2023-05-08 19:09:54 +0530

    one String Puzzle Solved

---
+++ src/com/appname/collections/HashMapTrickyQuestions.java
@@ -0,0 +1,15 @@
+package com.appname.collections;
+
+import java.util.HashMap;
+
+public class HashMapTrickyQuestions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		HashMap<String, Integer> map = new HashMap<String,Integer>();
+		
+		System.out.println("Value is "+map.get("a"));
+	}
+
+}
---
+++ src/com/appname/impprogs/StringPuzzle1_DeutscheBank.java
@@ -0,0 +1,39 @@
+package com.appname.impprogs;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * Write A Program to get char followed by count - maintain the insertion order
+ * e.g. aabbbccdaaaa should o/p a2b3c2d1a4
+ * Asked in Deutsche Bank Online Test
+ * @author rites
+ *
+ */
+
+public class StringPuzzle1_DeutscheBank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Scanner scan = new Scanner(System.in);
+
+		// String input = scan.nextLine();
+		String input = "aananansnnanansnnnnsnnsnsnsssss";
+		System.out.println(input);
+		int counter = 0;
+		for (int index = 0; index < input.length(); index++) {
+			counter++;
+			if (index == input.length() - 1) {
+				// Special handling for Last Char
+				System.out.print(input.charAt(index) + ""+counter);
+			} else {
+				//Not Last Char
+				if (input.charAt(index) != input.charAt(index + 1)) {
+					//Print Condition and Flush Counter
+					System.out.print(input.charAt(index) + ""+counter);
+					counter = 0;
+				} 
+			}
+		}
+	}
+}

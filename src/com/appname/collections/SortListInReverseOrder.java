db08f2642a9a72f3aee03d6b901bdc1b169344b0
Ritesh Mansukhani <riteshmansukhani@gmail.com>
2024-05-06 16:10:12 +0530

    Added more Java Programs and Selenium Programs

---
+++ src/com/appname/collections/SortListInReverseOrder.java
@@ -0,0 +1,16 @@
+package com.appname.collections;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class SortListInReverseOrder {
+
+	public static void main(String[] args) {
+	List<String> names = 
+		Arrays.asList("CAB", "TAB", "MOB", "NAB", "ROB", "BOB");
+    //Each character of string converted into a Unicode value
+	Collections.sort(names, (a, b) -> b.compareTo(a));
+	System.out.println(names);
+	}
+}

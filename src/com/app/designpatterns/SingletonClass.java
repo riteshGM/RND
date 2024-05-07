8605e32459d61a0ecd2d28d984573e5498ac76bf
riteshGM <riteshmansukhani@gmail.com>
2024-05-07 18:18:58 +0530

    Changed/Improved Singleton Sample

--- src/com/app/designpatterns/SingletonClass.java
+++ src/com/app/designpatterns/SingletonClass.java
@@ -1,7 +1,5 @@
 package com.app.designpatterns;
 
-import groovyjarjarantlr4.v4.parse.ANTLRParser.sync_return;
-
 public class SingletonClass {
 
 	//Private Constructor - Dont want Instance Creation by any outside Program
@@ -11,9 +9,12 @@ public class SingletonClass {
 		
 	}
 	
-	//Private Static Instance of Class
-	private static SingletonClass obj;
+	/**
+	 * Private - since we dont want direct access to instance
+	 * Static Only 1 Instance is created and passed on until teared down/nullified
+	 */
 	
+	private static SingletonClass obj;
 	
 	//Public Static getInstance Method
 	//Synchronized will Make it Threadsafe
@@ -21,9 +22,11 @@ public class SingletonClass {
 		if(obj == null) {
 			obj = new SingletonClass();
 		}
-		
 		return obj;
 	}
 	
+	public static synchronized void tearDown() {
+		obj = null;
+	}
 
 }
--- src/com/app/designpatterns/SingletonUsageProgram.java
+++ src/com/app/designpatterns/SingletonUsageProgram.java
@@ -7,8 +7,15 @@ public class SingletonUsageProgram {
 		System.out.println(obj);
 		SingletonClass obj2 = SingletonClass.getInstnace();
 		System.out.println(obj2);
-		//obj.tearDown();
-		System.out.println(obj);
+		//Nullify Singleton Instance;
+		SingletonClass.tearDown();
+		System.out.println(SingletonClass.getInstnace());
+		System.out.println(SingletonClass.getInstnace());
+		System.out.println(SingletonClass.getInstnace());
+		SingletonClass.tearDown();
+		System.out.println(SingletonClass.getInstnace());
+		System.out.println(SingletonClass.getInstnace());
+		
 	}
 
 }

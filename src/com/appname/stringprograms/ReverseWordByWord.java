6342b57bebdf24dd4cfd96eca61e53d1c73aeec7
riteshGM <riteshmansukhani@gmail.com>
2024-09-18 17:43:33 +0530

    Improvised programs

--- src/com/appname/stringprograms/ReverseWordByWord.java
+++ src/com/appname/stringprograms/ReverseWordByWord.java
@@ -6,24 +6,7 @@ public class ReverseWordByWord {
 
 		String given = "Learning oops concepts in Java";
 
-		String reversed = "";
-/*
-		// Create words Array
-		String[] words = given.split(" ");
-		for (int k = 0; k < words.length; k++) {
-			String wordsArray = "";
-			wordsArray = wordsArray + words[k] + " ";
-
-			// For each word Create characters Array
-			char[] chArray = wordsArray.toCharArray();
-
-			// Print the characters of each word from last to first
-			for (int j = chArray.length - 1; j >= 0; j--) {
-				reversed = reversed + chArray[j];
-			}
-		}
-		
-	*/	
+		String reversed = "";	
 		String[] inputArr = given.split(" ");
 		
 		for(int k=inputArr.length-1;k>=0;k-- ) {
@@ -38,10 +21,8 @@ public class ReverseWordByWord {
 		if(input.isEmpty()) {
 			return input;
 		}
-		
-		return input.substring(input.length()-1)+
+		return input.charAt(input.length()-1)+
 				reverse(input.substring(0,input.length()-1));
 	}
-	
 	
 }
--- src/com/appname/stringprograms/SplitStringifNumericFound.java
+++ src/com/appname/stringprograms/SplitStringifNumericFound.java
@@ -16,6 +16,10 @@ aacha
 
 import java.util.Scanner;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+import org.checkerframework.checker.index.qual.SubstringIndexBottom;
+
 public class SplitStringifNumericFound {
 
 	public static void main(String[] args) {
@@ -23,22 +27,17 @@ public class SplitStringifNumericFound {
 		//Scanner scanner = new Scanner(System.in);
 		//String input = scanner.nextLine();
 		
-		String input = "aacha1ahsndh4hhhahdgdhh0kkdjdk23";
-		char[] array = input.toCharArray();
-		String temp="";
-		for(int index = 0 ; index < array.length ; index++) {
-			
-			//Check if Char is Digit
-			if(Character.isDigit(array[index])) {
-				System.out.println(temp); //Print Current Cache
-				temp=""; //Reset Temp
-				temp=temp+array[index];
-			}else {
-				temp=temp+array[index];
-			}
-			
-			if(index==array.length-1) {
-				System.out.println(temp);
+		String input = "aacha1ahsndh4hhhahdgdhh0kkdjdk23asda";
+		System.out.println("Original String "+input);
+		while(!input.isEmpty()) {
+			for(int i = 0 ; i< input.length(); i++) {
+				if(Character.isDigit(input.charAt(i)) | i==input.length()-1) {
+					System.out.println("["+input.substring(0,i)+"]");
+					//Reset Input to further remaining String
+					input = input.substring(i+1);
+					//System.out.println("Remaining String "+input);
+					break; // this terminates current for loop
+				}
 			}
 		}
 		
